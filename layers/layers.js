0891d24fd53f98345b76e6ba44dcb07c2cbe9e94
RoyZhoy <41718331+RoyZhoy@users.noreply.github.com>
2018-09-12 09:28:13 +1200

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,16 @@
+var wms_layers = [];
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap_0',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: [new ol.Attribution({html: '<a href=""></a>'})],
+                url: 'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+
+
+var layersList = [lyr_OpenStreetMap_0];
